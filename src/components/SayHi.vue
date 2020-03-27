ac3c63aef93c55c113745a5ab3812b4c3e3f7cf2
chen <120184134@qq.com>
2020-03-27 10:10:12 +0800

    Add files via upload
    初始

---
+++ src/components/SayHi.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hi">
+    Hi Vue!
+    {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
+}
+</script>
+
+<style>
+.hi {
+  color : red;
+  font-size : 33px;
+  text-align: left;
+}
+</style>
